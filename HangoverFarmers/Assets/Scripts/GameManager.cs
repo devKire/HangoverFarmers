5a0105b4bb52cc15cbf810dcb93bff52d1e962f3
zek409080 <eduardoluiz08092004@gmail.com>
2024-05-20 11:21:54 -0300

    style

--- HangoverFarmers/Assets/Scripts/GameManager.cs
+++ HangoverFarmers/Assets/Scripts/GameManager.cs
@@ -5,23 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-       
-    }
-
     public void updateScene(string Main)
     {
         SceneManager.LoadScene(Main);
-
     }
-
     public void doExitGame()
     {
         Application.Quit();
